501488119a4b196f1798e3e6fcb3aa2b4ec8c889
Mike McColm <mike.mccolm28@gmail.com>
2019-11-15 17:38:43 -0500

    import and process data

---
+++ cleanData.py
@@ -0,0 +1,24 @@
+import csv,random
+import pandas as pd
+
+data = []
+
+def importData():
+    #1367 is the label
+    #numerical data ends at 1366
+    #not bringing in booleans rn
+    global data
+    data = pd.read_csv("shotDataSample.csv")
+
+def processData():
+    global data
+    #print (data[0][1]) #row, column
+    toDelete = [1,3,6,7,9,18,19,20,23,24,25,26,27,28,29,33,34,36,37,41,42,43,44,45] #columns to delete
+    toCopy = [0,2,4,5,8,10,11,12,13,14,15,16,17,21,22,30,31,32,35,38,39,40]
+    #for i in toDelete
+    data = data.drop(data.columns[toDelete],axis = 1)
+    print (data)
+
+
+importData()
+processData()
