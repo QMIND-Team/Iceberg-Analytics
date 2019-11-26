5df70e626ee919a52d94799028f5a54b0aa00adf
andrewsimonds14 <16avs6@queensu.ca>
2019-11-25 22:30:16 -0500

    Changed some formatting

--- cleanData.py
+++ cleanData.py
@@ -1,20 +1,23 @@
-import csv,random
+import csv, random
 import pandas as pd
 
 data = []
+
 
 def importData():
     global data
     data = pd.read_csv("data/shotDataSample.csv")
 
+
 def processData():
     global data
-    #print (data[0][1]) #row, column
-    toDelete = [1,3,6,7,9,18,19,20,23,24,25,26,27,28,29,33,34,36,37,41,42,43,44,45] #columns to delete
-    toCopy = [0,2,4,5,8,10,11,12,13,14,15,16,17,21,22,30,31,32,35,38,39,40]
-    #for i in toDelete
-    data = data.drop(data.columns[toDelete],axis = 1)
-    print (data)
+    # print (data[0][1]) #row, column
+    toDelete = [1, 3, 6, 7, 9, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 33, 34, 36, 37, 41, 42, 43, 44,
+                45]  # columns to delete
+    toCopy = [0, 2, 4, 5, 8, 10, 11, 12, 13, 14, 15, 16, 17, 21, 22, 30, 31, 32, 35, 38, 39, 40]
+    # for i in toDelete
+    data = data.drop(data.columns[toDelete], axis=1)
+    print(data)
 
 
 importData()
--- shotLocation.py
+++ shotLocation.py
@@ -1,58 +1,60 @@
-import csv,random
+import csv, random
 import pandas as pd
 
 data = []
+
 
 def importData():
     global data
     data = pd.read_csv("data/output.csv")
 
+
 def processData():
     global data
     i = 0
-    for i in range (0,data.shape[0]):
-        x = data.iloc[i,8]
-        y = data.iloc[i,9]
+    for i in range(0, data.shape[0]):
+        x = data.iloc[i, 8]
+        y = data.iloc[i, 9]
         if x < 0:
             x = -x
             y = -y
         if y > 690:
-            if 2650 >= x and x > 2050:
-                data.iloc[i,14] = "A1"
-            elif  2050 >= x and x > 1400:
-                data.iloc[i,14] = "A2"
-            elif 1400 >= x and x >= 750:
-                data.iloc[i,14] = "A3"
+            if 2650 >= x > 2050:
+                data.iloc[i, 14] = "A1"
+            elif 2050 >= x > 1400:
+                data.iloc[i, 14] = "A2"
+            elif 1400 >= x >= 750:
+                data.iloc[i, 14] = "A3"
             else:
-                data.iloc[i,14] = "BAD"
+                data.iloc[i, 14] = "BAD"
         elif 690 >= y > 0:
-            if 2650 >= x and x > 2050:
-                data.iloc[i,14] = "B1"
-            elif  2050 >= x and x > 1400:
-                data.iloc[i,14] = "B2"
-            elif 1400 >= x and x >= 750:
-                data.iloc[i,14] = "B3"
+            if 2650 >= x > 2050:
+                data.iloc[i, 14] = "B1"
+            elif 2050 >= x > 1400:
+                data.iloc[i, 14] = "B2"
+            elif 1400 >= x >= 750:
+                data.iloc[i, 14] = "B3"
             else:
-                data.iloc[i,14] = "BAD"
+                data.iloc[i, 14] = "BAD"
         elif 0 >= y > -690:
-            if 2650 >= x and x > 2050:
-                data.iloc[i,14] = "C1"
-            elif  2050 >= x and x > 1400:
-                data.iloc[i,14] = "C2"
-            elif 1400 >= x and x >= 750:
-                data.iloc[i,14] = "C3"
+            if 2650 >= x > 2050:
+                data.iloc[i, 14] = "C1"
+            elif 2050 >= x > 1400:
+                data.iloc[i, 14] = "C2"
+            elif 1400 >= x >= 750:
+                data.iloc[i, 14] = "C3"
             else:
-                data.iloc[i,14] = "BAD"
+                data.iloc[i, 14] = "BAD"
         else:
-                if 2650 >= x and x > 2050:
-                    data.iloc[i,14] = "D1"
-                elif  2050 >= x and x > 1400:
-                    data.iloc[i,14] = "D2"
-                elif 1400 >= x and x >= 750:
-                    data.iloc[i,14] = "D3"
-                else:
-                    data.iloc[i,14] = "BAD"
-    print (data["Shot location"])
+            if 2650 >= x > 2050:
+                data.iloc[i, 14] = "D1"
+            elif 2050 >= x > 1400:
+                data.iloc[i, 14] = "D2"
+            elif 1400 >= x >= 750:
+                data.iloc[i, 14] = "D3"
+            else:
+                data.iloc[i, 14] = "BAD"
+    print(data["Shot location"])
 
 
 importData()
