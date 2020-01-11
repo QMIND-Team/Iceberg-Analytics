d59c32bb3786108aa736ec61b587b05111093845
Mike McColm <mike.mccolm28@gmail.com>
2020-01-11 14:20:47 -0500

    starting tensorflow analysis

--- shotLocation.py
+++ shotLocation.py
@@ -1,5 +1,12 @@
 import csv, random, math
+import numpy as np
 import pandas as pd
+
+import tensorflow as tf
+
+from tensorflow import feature_column
+from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
 
 data = []
 
@@ -26,8 +33,28 @@ def angleCalc(x, y):
 
 def processData():
     global data
-    toDelete = [0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 15, 16]
-    # Shot X , Shot Y, ShotLocation, Rebound (T/F), ReboundX, Rebound Y
+    toDelete = [0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 15]
+    # 0 = Game id
+    # 1 = Home team name
+    # 2 = Away team name
+    # 3 = Event id
+    # 4 = Abs time
+    # 5 = Event
+    # 6 = Shooter team status
+    # 7 = Shooter name
+    # 8 = Shot x
+    # 9 = Shot y
+    # 10 = Goalie team status
+    # 11 = Goalie name
+    # 12 = Goalie x
+    # 13 = Goalie y
+    # 14 = Shot location
+    # 15 = Shot category
+    # 16 = Save type
+    # 17 = Rebound
+    # 18 = Rebound x
+    # 19 = Rebound y
+    # Current data: Shot X , Shot Y, ShotLocation, Rebound (T/F), ReboundX, Rebound Y
     data = data.drop(data.columns[toDelete], axis=1)
     i = 0
     angles = []
@@ -35,8 +62,8 @@ def processData():
     for i in range(0, data.shape[0]):
         x = data.iloc[i, 0]
         y = data.iloc[i, 1]
-        xAngle = data.iloc[i, 4]
-        yAngle = data.iloc[i, 5]
+        xAngle = data.iloc[i, 5]
+        yAngle = data.iloc[i, 6]
         # flip data for right side of net
         if x < 0:
             x = -x
@@ -49,8 +76,13 @@ def processData():
         newAngle = angleCalc(xAngle, yAngle)
         angles.append(newAngle)
         if math.isnan(newAngle):
-            # -1 = NO REBOUND
-            angleBins.append(-1)
+            if isinstance(data.iloc[i, 3], str):
+                # -1 = NO REBOUND - SAVE
+                angleBins.append(-1)
+            else:
+                # -1 = NO REBOUND - GOAL
+                 angleBins.append(-2)
+                 data.iloc[i,3] = "GOAL"
         else:
             angleBins.append(int(newAngle / 15))
         # calculate shooting bin
@@ -92,10 +124,63 @@ def processData():
                 data.iloc[i, 2] = "BAD"
     #print(data["Shot location"])
     # add new angles column
-    data.insert(6, "Rebound Angle", angles, True)
-    data.insert(7, "Rebound Bin", angleBins, True)
+    data.insert(7, "Rebound_Angle", angles, True)
+    data.insert(8, "Rebound_Bin", angleBins, True)
     print(data)
+
+def analyzeData():
+    global data
+    data.fillna('', inplace=True)
+    train,test = train_test_split(data, test_size = 0.2)
+    train,val = train_test_split(data, test_size = 0.2)
+    batch_size = 32
+    train_ds = df_to_dataset(train, batch_size = batch_size)
+    test_ds = df_to_dataset(test, shuffle = False, batch_size = batch_size)
+    val_ds = df_to_dataset(val, shuffle = False, batch_size = batch_size)
+
+    feature_columns = []
+
+    shotBin = feature_column.categorical_column_with_vocabulary_list(
+        key = 'Shot_location',
+        vocabulary_list= [str("A1"),"A2","A3","B1","B2","B3","C1","C2","C3","D1","D2","D3","BAD"])
+    shotBin_one_hot = feature_column.indicator_column(shotBin)
+    feature_columns.append(shotBin_one_hot)
+
+    for f in feature_columns:
+        train[f] = train[f].astype(str)   
+        test[f] = test[f].astype(str) 
+
+    feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
+
+    model = tf.keras.Sequential([
+        feature_layer,
+        layers.Dense(128, activation='relu'),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(optimizer='adam',
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+
+    model.fit(train_ds,
+            validation_data=val_ds,
+            epochs=5)
+
+    loss,accuracy = model.evaluate(test_ds)
+
+
+
+def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+    dataframe = dataframe.copy()
+    labels = dataframe.pop("Rebound_Bin")
+    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+    if shuffle:
+        ds = ds.shuffle(buffer_size = len(dataframe))
+    ds = ds.batch(batch_size)
+    return ds
 
 
 importData()
 processData()
+analyzeData()
