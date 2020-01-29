a863f90d1314d8220ee6da526d3400c6768e8c59
andrewsimonds14 <16avs6@queensu.ca>
2020-01-28 21:28:59 -0500

    Added mouse tracking

--- gui.py
+++ gui.py
@@ -10,4 +10,12 @@ image = tk.PhotoImage(file="./assets/HockeyRinkZone.png")
 label = tk.Label(image=image)
 label.pack()
 
+# Display x-y of mouse
+# Will use this to detect where puck is placed
+def motion(event):
+    x, y = event.x, event.y
+    print('{}, {}'.format(x, y))
+
+window.bind('<Motion>', motion)
+
 window.mainloop()
