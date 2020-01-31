eed4f38f8534387b6fd261ee9edc64c91eaac6c3
andrew.simonds14 <andrew.simonds14@hotmail.com>
2020-01-31 12:17:56 -0500

    Checking onClick functionality

--- gui.py
+++ gui.py
@@ -1,14 +1,18 @@
 import tkinter as tk
 
+# Setup tkinter
 window = tk.Tk()
-
 window.title("Rebound Predictor Tool")
-
 label = tk.Button(window, text = "Rebound tool").pack()
 
+# Display image
 image = tk.PhotoImage(file="./assets/HockeyRinkZone.png")
 label = tk.Label(image=image)
 label.pack()
+
+# Define globals
+puckLocationX = 0
+puckLocationY = 0
 
 # Display x-y of mouse
 # Will use this to detect where puck is placed
@@ -16,6 +20,19 @@ def motion(event):
     x, y = event.x, event.y
     print('{}, {}'.format(x, y))
 
+def onClick(event):
+    global puckLocationX, puckLocationY
+    print ("Clicked at", event.x, event.y)
+    puckLocationX, puckLocationY = event.x, event.y
+
+def checkPuck(event):
+    print ("Pressed", repr(event.char))
+    print(puckLocationX, puckLocationY)
+
+
+
 window.bind('<Motion>', motion)
+window.bind('<Button-1>', onClick)
+window.bind("<Key>", checkPuck)
 
 window.mainloop()
